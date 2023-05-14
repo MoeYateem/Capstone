b980efd3ac7799debae65507a0ecb59120454287
Mohamad Yateem <98651638+MoeYateem@users.noreply.github.com>
2023-05-15 02:45:04 +0300

    Add files via upload

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,8 +1,38 @@
 <script setup>
 import { ref } from "vue";
 import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+import axios from "axios";
 import { Switch, SwitchGroup, SwitchLabel } from "@headlessui/vue";
+const newcontact = ref({ email: "", phone_number: 0,fname: "",lname: "",company: "",message: "", });
+const submitContact= async () => {
+  const url = 'http://localhost/Tunezz/Tunezz/APIs/appfunctions/contact.php';
+  
+  // const email = userEmail.value;
+  const fname= newcontact.value.fname;
+  const lname= newcontact.value.lname;
+  const company= newcontact.value.company;
+  const phone_number= newcontact.value.phone_number;
+  const message= newcontact.value.message;
+  const email= newcontact.value.email;
+  const data = {
+    // cartadd: true,
+    submit:true,
+    email: email,
+    fname:fname,
+    lname:lname,
+    company:company,
+    phone_number:phone_number,
+    message:message
+  };
+  console.log("Data to be sent:", data);
 
+  try {
+    const response = await axios.post(url, new URLSearchParams(data));
+    console.log("Response:", response.data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 const agreed = ref(false);
 </script>
 
@@ -16,17 +46,21 @@ const agreed = ref(false);
         Aute magna irure deserunt veniam aliqua magna enim voluptate.
       </p>
     </div>
-    <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+    <form @submit.prevent="submitContact">
       <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div>
           <label
             for="first-name"
+            
+
             class="block text-sm font-semibold leading-6 text-primary"
+            
             >First name</label
           >
           <div class="mt-2.5">
             <input
               type="text"
+              v-model="newcontact.fname"
               name="first-name"
               id="first-name"
               autocomplete="given-name"
@@ -44,6 +78,7 @@ const agreed = ref(false);
             <input
               type="text"
               name="last-name"
+              v-model="newcontact.lname"
               id="last-name"
               autocomplete="family-name"
               class="block w-full bg-gray-100 bg-opacity-70 focus:bg-white focus:ring-primary rounded-md border-0 px-3.5 py-2 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
@@ -60,6 +95,7 @@ const agreed = ref(false);
             <input
               type="text"
               name="company"
+              v-model="newcontact.company"
               id="company"
               autocomplete="organization"
               class="block bg-gray-100 bg-opacity-70 focus:bg-white focus:ring-primary w-full rounded-md border-0 px-3.5 py-2 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
@@ -77,6 +113,7 @@ const agreed = ref(false);
               type="email"
               name="email"
               id="email"
+              v-model="newcontact.email"
               autocomplete="email"
               class="block bg-gray-100 bg-opacity-70 focus:bg-white focus:ring-primary w-full rounded-md border-0 px-3.5 py-2 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
             />
@@ -93,6 +130,7 @@ const agreed = ref(false);
               <label for="country" class="sr-only">Country</label>
               <select
                 id="country"
+                v-model="newcontact.country"
                 name="country"
                 class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-1 sm:text-sm"
               >
@@ -109,6 +147,7 @@ const agreed = ref(false);
               type="tel"
               name="phone-number"
               id="phone-number"
+              v-model="newcontact.phone_number"
               autocomplete="tel"
               class="block w-full bg-gray-100 bg-opacity-70 focus:bg-white focus:ring-primary rounded-md border-0 px-3.5 py-2 pl-20 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
             />
@@ -124,6 +163,7 @@ const agreed = ref(false);
             <textarea
               name="message"
               id="message"
+              v-model="newcontact.message"
               rows="4"
               class="block w-full bg-gray-100 bg-opacity-70 focus:bg-white focus:ring-primary rounded-md border-0 px-3.5 py-2 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
             ></textarea>
